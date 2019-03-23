ddecf8d4cc84570a34b64ee93f6711bbb6bcfed6
Art <artelo.studio@gmail.com>
2019-03-23 00:22:42 +0000

    restricions permissions

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
 
   def index
     @places = Place.all
@@ -20,10 +20,18 @@ class PlacesController < ApplicationController
 
     def edit
       @place = Place.find(params[:id])
+
+      if @place.user != current_user
+    return render plain: 'Not Allowed', status: :forbidden
+  end
   end
 
 def update
    @place = Place.find(params[:id])
+   if @place.user != current_user
+    return render plain: 'Not Allowed', status: :forbidden
+  end
+
   @place.update_attributes(place_params)
   redirect_to root_path
 
@@ -31,10 +39,9 @@ end
 
 def destroy
     @place = Place.find(params[:id])
-  @place.destroy
-  redirect_to root_path
-
-  end
+    @place.destroy
+    redirect_to root_path
+end
 
   private
 
